ef0ad202e839ef14bfd5a3619f937e721341680d
MiguelAlmeidaJ <jmalmeida700@gmail.com>
2026-09-03 10:00:55 -0300

    fix: ajustado pm2 para host aberto

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 4333,
-        HOST: '127.0.0.1',
+        HOST: '0.0.0.0',
         PM2_MANAGER_PROCESS_NAME: 'pm2-manager',
         PM2_MANAGER_BACKUP_KEEP: 30
       }
