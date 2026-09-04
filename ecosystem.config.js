3797bc2e073bf6aa70aff6cdbfbe1a43c3712e4a
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-04 14:05:23 -0300

    chore: run PM2 Manager through bootstrap

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'pm2-manager',
-      script: './server.js',
+      script: './bootstrap.js',
       instances: 1,
       autorestart: true,
       watch: false,
@@ -12,7 +12,11 @@ module.exports = {
         PORT: 4333,
         HOST: '0.0.0.0',
         PM2_MANAGER_PROCESS_NAME: 'pm2-manager',
-        PM2_MANAGER_BACKUP_KEEP: 30
+        PM2_MANAGER_BACKUP_KEEP: 30,
+        PM2_MANAGER_SCAN_MAX_DEPTH: 7,
+        PM2_MANAGER_SCAN_MAX_RESULTS: 500,
+        PM2_MANAGER_SCAN_TIMEOUT_MS: 15000,
+        PM2_MANAGER_TERMINAL_TIMEOUT_MS: 8000
       }
     }
   ]
