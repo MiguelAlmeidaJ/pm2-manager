b9c79bf39db02eaebb2b694e09b6beeb33f70344
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-04 14:30:05 -0300

    fix: add Windows project root to tools defaults

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,14 @@
+const os = require('os');
+const path = require('path');
 const { migrateLegacyAuthFile } = require('./lib/auth-migration');
+
+if (process.platform === 'win32' && !process.env.PM2_MANAGER_SCAN_ROOTS) {
+  process.env.PM2_MANAGER_SCAN_ROOTS = [
+    os.homedir(),
+    path.dirname(process.cwd()),
+  ].join(',');
+}
+
 const { installExpressExtension } = require('./lib/tools-extension');
 
 const migration = migrateLegacyAuthFile();
